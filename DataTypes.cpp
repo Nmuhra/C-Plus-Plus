ce36270e7a4cdc39eeafbadad900dfd0898c39ba
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-11-24 13:18:43 +0200

    Create DataTypes.cpp

---
+++ DataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using  namespace std;
+
+int main()
+{
+  char grade = 'A';
+  
+  string phrase = "Nabegh";
+  
+  int number = 16;
+
+  //double is used for specific numbers including many decimals
+  double GPA = 2.6;
+
+  // True/False
+  bool isRed = true;
+  return 0;
+  
+}
